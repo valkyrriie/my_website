e515001406725abe691f97891a96af80b97648d9
valkyrriie <161313243+valkyrriie@users.noreply.github.com>
2025-12-06 23:04:27 +0400

    Update settings.py

--- schedule/settings.py
+++ schedule/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1we0gp39bp(_9s+@uyqr(%c*!tkc0xb2-&xuh9b20jw^#+n00i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
